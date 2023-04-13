233d74c18b6840288569d3889580dc83c2525e1a
Muhammad Rafly <80822767+MRaflyH@users.noreply.github.com>
2023-04-13 19:45:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from matriks import user_login, user, candi, bahan_bangunan
 from command_input import run
 
 while True:
+    # hilangkan comment untuk print jika ingin melihat perubahan setiap input
+    print(user_login)
+    print(user)
+    # print(candi)
+    # print(bahan_bangunan)
     masukan = input(">>> ")
     if masukan == "stop":
         break
