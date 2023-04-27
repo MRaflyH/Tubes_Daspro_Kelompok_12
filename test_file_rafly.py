9dab7935336b4052465fb513af3e4112b794e57a
amaliap21 <116664738+amaliap21@users.noreply.github.com>
2023-04-27 12:36:21 +0700

    Delete Test

--- test_file_rafly.py
+++
@@ -1,9 +0,0 @@
-from custom_functions import *
-from matriks import user, max_user, candi, max_candi, bahan_bangunan, max_bahan_bangunan
-from random import randint
-
-array = [1, 2, 3, 4, 5, 6, 7, 8, None, None]
-max_array = 10
-elemen = 0
-
-print(custom_insert(elemen, array, max_array))
